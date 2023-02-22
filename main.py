98576d8104ae6e4f7d49ce7d124be4864ab26e15
Wallace Martins <108425466+IWMVI@users.noreply.github.com>
2023-02-22 02:59:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+Receba o valor de um depósito em poupança. Calcule e mostre o valor após 1 mês de aplicação sabendo que rende 1,3% a. m.
+
+import time
+
+deposito = float (input("Qual o valor que será depositado? "))
+tempo = int (input ("Quanto tempo ficará rendendo? "))
+
+
